06372c100c7a820c9b65f2c8b8d1b4739eac0050
jakeschroeder <jakeschroeder@newschool.edu>
2018-03-06 02:28:31 -0500

    implemented header

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -4,5 +4,8 @@ window.$ = $;
 
 $(document).ready(function(){
     console.log("Document is ready");
-    
+    $(".hamburger-wrapper").click(function(){
+        $(this).toggleClass('open');
+        $(".site-nav").toggleClass('open');
+	});
 });
