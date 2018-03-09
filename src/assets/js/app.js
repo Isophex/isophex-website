32b95b95dc78196a13a1a611d0fdc04b8fca2f32
Jake Schroeder <jakeschroeder8@gmail.com>
2018-03-08 21:33:55 -0500

    finished about and added work section

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -8,5 +8,13 @@ $(document).ready(function(){
         $(this).toggleClass('open');
         $(".site-nav").toggleClass('open');
         $("body").toggleClass("no-scroll");
-	});
+    });
+
+    // var $heliosContainer = $(".helios-case .left");
+    // var $heliosImg = $(".helios-case .image-wrapper");
+
+    // $heliosContainer.mouseenter(function(){
+    //     $heliosImg.addClass("upper");
+    // });
+
 });
