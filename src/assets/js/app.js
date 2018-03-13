d95f636f068c3dea390163cb5097b3b95cccd6b4
jakeschroeder <jakeschroeder@newschool.edu>
2018-03-12 21:59:11 -0400

    finished work and added contact plus the footer

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,4 +1,6 @@
 import $ from 'jquery';
+import Typed from 'typed.js';
+import Vivus from 'vivus';
 
 window.$ = $;
 
@@ -9,6 +11,42 @@ $(document).ready(function(){
         $(".site-nav").toggleClass('open');
         $("body").toggleClass("no-scroll");
     });
+
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top - 80,
+              
+            }, 500, function(){
+                $(".hamburger-wrapper").removeClass('open');
+                $(".site-nav").removeClass('open');
+                $("body").removeClass("no-scroll");
+            });
+            return false;
+          }
+        }
+    });
+
+    var typed = new Typed('.type-here', {
+        stringsElement: '#typed-strings',
+        cursorChar: ' |',
+        loop: false,
+        typeSpeed: 200,
+        backSpeed: 0,
+        fadeOut: true,
+        onComplete: function(self) { 
+            $(".typed-cursor").css({
+                "animation-iteration-count": "initial",
+            });
+        },
+    });
+
+    new Vivus('twisty-graphic', {duration: 200});
+    new Vivus('ecommerce-graphic', {duration: 200});
+    new Vivus('blog-graphic', {duration: 150});
 
     // var $heliosContainer = $(".helios-case .left");
     // var $heliosImg = $(".helios-case .image-wrapper");
